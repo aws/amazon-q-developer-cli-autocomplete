60e4db074c690cce3db08ca06542dfb87993859e
Felix dingfeli <dingfeli@amazon.com>
2025-06-24 11:24:43 -0700

    fixes typo

--- crates/chat-cli/src/cli/chat/cli/profile.rs
+++ crates/chat-cli/src/cli/chat/cli/profile.rs
@@ -94,7 +94,7 @@ impl ProfileSubcommand {
                     session.stderr,
                     style::SetForegroundColor(Color::Yellow),
                     style::Print(format!(
-                        "Persona / Profile persistance has been disabled. To perform any CRUD on persona / profile, use the default persona under {} as example",
+                        "Persona / Profile persistence has been disabled. To perform any CRUD on persona / profile, use the default persona under {} as example",
                         global_path
                     )),
                     style::SetAttribute(Attribute::Reset)
