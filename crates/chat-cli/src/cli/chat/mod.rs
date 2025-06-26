80b952e989d65dfca70610a842f2e2b750fea084
YIFAN LIU <evannliu@amazon.com>
2025-06-26 15:00:54 -0700

    fix help tip fmt

--- crates/chat-cli/src/cli/chat/mod.rs
+++ crates/chat-cli/src/cli/chat/mod.rs
@@ -353,11 +353,9 @@ const ROTATING_TIPS: [&str; 16] = [
 
 const GREETING_BREAK_POINT: usize = 80;
 
-const POPULAR_SHORTCUTS: &str = color_print::cstr! {"<black!><green!>/help</green!> all commands  <em>•</em>
-<green!>ctrl + j</green!> new lines  <em>•</em>  
-<green!>ctrl + s</green!> fuzzy search  <em>•</em>  
-<green!>ctrl + f</green!> accept completion
-</black!>"};
+const POPULAR_SHORTCUTS: &str = color_print::cstr! {
+"<black!><green!>/help</green!> all commands                      <em>•</em>  <green!>ctrl + j</green!> new lines
+<green!>ctrl + s</green!> fuzzy search                   <em>•</em>  <green!>ctrl + f</green!> accept completion</black!>"};
 const SMALL_SCREEN_POPULAR_SHORTCUTS: &str = color_print::cstr! {"<black!><green!>/help</green!> all commands
 <green!>ctrl + j</green!> new lines
 <green!>ctrl + s</green!> fuzzy search
